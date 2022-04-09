76a5fd9fb31e0e2a0173384e3490b9bf42a935a4
Noah Torrance <noahtorrance27@gmail.com>
2022-04-09 16:39:28 +0100

    Change keybinds

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const char font[]        = "monospace:size=9";
+static const char font[]        = "Liberation Mono:size=10";
 static const char* normbgcolor  = "#222222";
 static const char* normfgcolor  = "#cccccc";
 static const char* selbgcolor   = "#555555";
@@ -20,8 +20,8 @@ static       Bool urgentswitch  = False;
  * then the current position is changed + newposition. If npisrelative
  * is False, then newposition is an absolute position.
  */
-static int  newposition   = 0;
-static Bool npisrelative  = False;
+static int  newposition   = 1;
+static Bool npisrelative  = True;
 
 #define SETPROP(p) { \
         .v = (char *[]){ "/bin/sh", "-c", \
@@ -33,34 +33,39 @@ static Bool npisrelative  = False;
         } \
 }
 
-#define MODKEY ControlMask
+#define MODKEY Mod1Mask
 static Key keys[] = {
-	/* modifier             key        function     argument */
-	{ MODKEY|ShiftMask,     XK_Return, focusonce,   { 0 } },
-	{ MODKEY|ShiftMask,     XK_Return, spawn,       { 0 } },
-
-	{ MODKEY|ShiftMask,     XK_l,      rotate,      { .i = +1 } },
-	{ MODKEY|ShiftMask,     XK_h,      rotate,      { .i = -1 } },
-	{ MODKEY|ShiftMask,     XK_j,      movetab,     { .i = -1 } },
-	{ MODKEY|ShiftMask,     XK_k,      movetab,     { .i = +1 } },
-	{ MODKEY,               XK_Tab,    rotate,      { .i = 0 } },
-
-	{ MODKEY,               XK_grave,  spawn,       SETPROP("_TABBED_SELECT_TAB") },
-	{ MODKEY,               XK_1,      move,        { .i = 0 } },
-	{ MODKEY,               XK_2,      move,        { .i = 1 } },
-	{ MODKEY,               XK_3,      move,        { .i = 2 } },
-	{ MODKEY,               XK_4,      move,        { .i = 3 } },
-	{ MODKEY,               XK_5,      move,        { .i = 4 } },
-	{ MODKEY,               XK_6,      move,        { .i = 5 } },
-	{ MODKEY,               XK_7,      move,        { .i = 6 } },
-	{ MODKEY,               XK_8,      move,        { .i = 7 } },
-	{ MODKEY,               XK_9,      move,        { .i = 8 } },
-	{ MODKEY,               XK_0,      move,        { .i = 9 } },
-
-	{ MODKEY,               XK_q,      killclient,  { 0 } },
-
-	{ MODKEY,               XK_u,      focusurgent, { 0 } },
-	{ MODKEY|ShiftMask,     XK_u,      toggle,      { .v = (void*) &urgentswitch } },
-
-	{ 0,                    XK_F11,    fullscreen,  { 0 } },
+    /* modifier             key             function        argument */
+	{ MODKEY|ShiftMask,     XK_Return,      focusonce,      { 0 } },
+	{ MODKEY|ShiftMask,     XK_Return,      spawn,          { 0 } },
+	{ MODKEY|ShiftMask,     XK_o,           rotate,         { .i = +1 } },
+	{ MODKEY|ShiftMask,     XK_y,           rotate,         { .i = -1 } },
+	{ MODKEY|ShiftMask,     XK_n,           movetab,        { .i = -1 } },
+	{ MODKEY|ShiftMask,     XK_e,           movetab,        { .i = +1 } },
+	{ MODKEY,               XK_Tab,         rotate,         { .i = 0 } },
+	{ MODKEY,               XK_u,           focusurgent,    { 0 } },
+	{ MODKEY|ShiftMask,     XK_u,           toggle,         { .v = (void*) &urgentswitch } },
+	{ 0,                    XK_F11,         fullscreen,     { 0 } },
+	{ MODKEY|ShiftMask,     XK_q,           killclient,     { 0 } },
+	{ MODKEY,               XK_grave,       spawn,          SETPROP("_TABBED_SELECT_TAB") },
+	{ MODKEY,               XK_1,           move,           { .i = 0 } },
+	{ MODKEY,               XK_2,           move,           { .i = 1 } },
+	{ MODKEY,               XK_3,           move,           { .i = 2 } },
+	{ MODKEY,               XK_4,           move,           { .i = 3 } },
+	{ MODKEY,               XK_5,           move,           { .i = 4 } },
+	{ MODKEY,               XK_6,           move,           { .i = 5 } },
+	{ MODKEY,               XK_7,           move,           { .i = 6 } },
+	{ MODKEY,               XK_8,           move,           { .i = 7 } },
+	{ MODKEY,               XK_9,           move,           { .i = 8 } },
+	{ MODKEY,               XK_0,           move,           { .i = 9 } },
+    { MODKEY,               XK_exclam,      move,           { .i = 0 } },
+	{ MODKEY,               XK_quotedbl,    move,           { .i = 1 } },
+	{ MODKEY,               XK_sterling,    move,           { .i = 2 } },
+	{ MODKEY,               XK_dollar,      move,           { .i = 3 } },
+	{ MODKEY,               XK_percent,     move,           { .i = 4 } },
+	{ MODKEY,               XK_asciicircum, move,           { .i = 5 } },
+	{ MODKEY,               XK_ampersand,   move,           { .i = 6 } },
+	{ MODKEY,               XK_asterisk,    move,           { .i = 7 } },
+	{ MODKEY,               XK_parenleft,   move,           { .i = 8 } },
+	{ MODKEY,               XK_parenright,  move,           { .i = 9 } },
 };
